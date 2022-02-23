1386edffef674c2c49ca9541d906db18ebbb4afe
Manjot08 <Manjotsingh15899@Gmail.com>
2022-02-24 00:25:43 +0530

    Updated

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from qrcode import *
+data=None
+def INDEX(request):
+    global data
+    if request.method=="POST":
+        data=request.POST['data']
+        img=make(data)
+        img.save("static/image/test.png")
+    else:
+        pass
+    return render(request,"index.html",{'data':data})
